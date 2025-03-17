fa74baaf45aac142d6ed83a29e8c2794d03d42cb
Sebastian <106036+schoeller@users.noreply.github.com>
2025-03-17 09:45:15 +0100

    Update urls.py

--- macro_converter/urls.py
+++ macro_converter/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.contrib import admin
 from django.urls import path
 
